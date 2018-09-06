78606db3adaf37698706db3768e43485d927ab31
Олег Максименко <olegmaksimenko@MacBook-Pro-Oleg.local>
2018-09-07 01:11:34 +0300

    timersetup

--- mail_task_objective/TweetViewController.m
+++ mail_task_objective/TweetViewController.m
@@ -12,26 +12,25 @@
 
 @end
 
+
+
 @implementation TweetViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    [NSTimer scheduledTimerWithTimeInterval:3.0 target:self selector:@selector (testtimer) userInfo:nil repeats:YES];
 }
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+
+- (void)testtimer {
+    NSLog(@"lets go timer!");
 }
-*/
 
 @end
