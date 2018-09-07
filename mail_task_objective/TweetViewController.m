09c4eb2787448f69afac580f3f18ce276e5bf3bb
Олег Максименко <olegmaksimenko@MacBook-Pro-Oleg.local>
2018-09-08 00:46:28 +0300

    network_started

--- mail_task_objective/TweetViewController.m
+++ mail_task_objective/TweetViewController.m
@@ -10,15 +10,24 @@
 
 @interface TweetViewController ()
 
+@property (weak, nonatomic) IBOutlet UITableView *table_tweet;
+@property (weak, nonatomic) IBOutlet UILabel *time_label;
+@property (weak, nonatomic) IBOutlet UITextField *text_field;
+@property (weak, nonatomic) IBOutlet UIButton *search_btn;
+
 @end
 
-
+int timeSec = 0;
+NSTimer *timer;
 
 @implementation TweetViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [NSTimer scheduledTimerWithTimeInterval:3.0 target:self selector:@selector (testtimer) userInfo:nil repeats:YES];
+    
+    [self StartTimer];
+    
+    [self parse];
 }
 
 
@@ -29,8 +38,46 @@
 
 
 
-- (void)testtimer {
-    NSLog(@"lets go timer!");
+-(void) StartTimer {
+    timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(timerTick:) userInfo:nil repeats:YES];
+    [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSDefaultRunLoopMode];
 }
+
+
+
+- (void) timerTick:(NSTimer *)timer {
+    timeSec++;
+    if (timeSec == 15)
+    {
+        timeSec = 0;
+    }
+    
+    NSString* timeNow = [NSString stringWithFormat:@"%02d", timeSec];
+    
+    _time_label.text= timeNow;
+}
+
+
+
+- (void) parse {
+    
+    NSURL *tutorialsUrl = [NSURL URLWithString:@"https://twitter.com/oleg02171931"];
+    NSData *tutorialsHtmlData = [NSData dataWithContentsOfURL:tutorialsUrl];
+    NSString *strData = [[NSString alloc]initWithData:tutorialsHtmlData encoding:NSUTF8StringEncoding];
+        NSLog(@"%@", strData);
+    /*dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        
+        [NSThread sleepForTimeInterval:3];
+        int i = arc4random() % 100;
+        
+        // update UI on the main thread
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.title = [[NSString alloc]initWithFormat:@"Result: %d", i];
+        });
+        
+    });*/
+}
+
+
 
 @end
